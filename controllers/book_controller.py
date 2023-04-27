138d8256927271b850e8244c9fdbd0893dae46c4
Stuart <stuartrure@gmail.com>
2023-04-27 09:36:21 +0100

    fixed code

--- controllers/book_controller.py
+++ controllers/book_controller.py
@@ -5,14 +5,14 @@ from flask import Blueprint
 
 import repositories.book_repository as book_repo
 
-tasks_blueprint = Blueprint("books", __name__)
+books_blueprint = Blueprint("books", __name__)
 
-@tasks_blueprint.route("/books")
+@books_blueprint.route("/books")
 def books ():
     books = book_repo.select_all()
     return render_template("books.jinja", books_to_display = books)
 
-@tasks_blueprint.route('/books/delete/<id>', methods=['POST'])
+@books_blueprint.route('/books/delete/<id>', methods=['POST'])
 def delete_book(id):
     book_repo.delete_by_id(int(id))
     return redirect('/books')
